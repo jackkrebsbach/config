a8cc4b5ca9cc300b9164c17b0811e4af4aa95f04
Jack Krebsbach <krebsbachjackson@gmail.com>
2024-12-27 16:57:56 -0500

    add reverse search latex

--- lua/config/plugins/vimtex.lua
+++ lua/config/plugins/vimtex.lua
@@ -5,12 +5,26 @@ return {
     init = function()
       vim.g.vimtex_view_method = "skim"
       vim.g.vimtex_compiler_method = 'tectonic'
+
       -- Command to compile on write
       vim.api.nvim_create_autocmd("BufWritePost", {
 	pattern = "*.tex",
 	callback = function()
 	  vim.cmd("VimtexCompile")
 	end,
+      })
+
+      -- PDF to Neovim Search CMD+Shift click
+      local function write_server_name()
+	local nvim_server_file = vim.fn.has('win32') == 1 and vim.fn.getenv('TEMP') or '/tmp'
+	nvim_server_file = nvim_server_file .. '/vimtexserver.txt'
+	vim.fn.writefile({ vim.v.servername }, nvim_server_file)
+      end
+
+      vim.api.nvim_create_autocmd('FileType', {
+	group = vim.api.nvim_create_augroup('vimtex_common', { clear = true }),
+	pattern = 'tex',
+	callback = write_server_name,
       })
     end,
   }
