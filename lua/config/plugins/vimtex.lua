1a4b190f06fd4d0fe16c1174327011df102a0680
Jack Krebsbach <krebsbachjackson@gmail.com>
2024-12-28 09:41:41 -0500

    remove custom reverse search

--- lua/config/plugins/vimtex.lua
+++ lua/config/plugins/vimtex.lua
@@ -13,19 +13,6 @@ return {
           vim.cmd("VimtexCompile")
         end,
       })
-
-      -- PDF to Neovim Search CMD+Shift click
-      local function write_server_name()
-        local nvim_server_file = vim.fn.has('win32') == 1 and vim.fn.getenv('TEMP') or '/tmp'
-        nvim_server_file = nvim_server_file .. '/vimtexserver.txt'
-        vim.fn.writefile({ vim.v.servername }, nvim_server_file)
-      end
-
-      vim.api.nvim_create_autocmd('FileType', {
-        group = vim.api.nvim_create_augroup('vimtex_common', { clear = true }),
-        pattern = 'tex',
-        callback = write_server_name,
-      })
     end,
   }
 }
