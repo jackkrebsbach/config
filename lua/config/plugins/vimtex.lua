cf4d43f5347815d084e2cbb31ab7d58a2de51d11
Jack Krebsbach <krebsbachjackson@gmail.com>
2024-12-28 13:41:48 -0500

    readd reverse search

--- lua/config/plugins/vimtex.lua
+++ lua/config/plugins/vimtex.lua
@@ -4,6 +4,19 @@ return {
     lazy = false,
     init = function()
       vim.g.vimtex_view_method = "skim"
+
+      -- PDF to Neovim Search CMD+Shift click
+      local function write_server_name()
+        local nvim_server_file = vim.fn.has('win32') == 1 and vim.fn.getenv('TEMP') or '/tmp'
+        nvim_server_file = nvim_server_file .. '/vimtexserver.txt'
+        vim.fn.writefile({ vim.v.servername }, nvim_server_file)
+      end
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('vimtex_common', { clear = true }),
+        pattern = 'tex',
+        callback = write_server_name,
+      })
       -- vim.g.vimtex_compiler_method = 'tectonic'
       --[[ vim.api.nvim_create_autocmd("BufWritePost", {
         pattern = "*.tex",
