57a8aefa0b51b4b41a493161afaaed20a1d1e21e
Jack Krebsbach <krebsbachjackson@gmail.com>
2024-12-30 11:29:11 -0500

    clear autocmd

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd("BufWritePost", {
-  group = vim.api.nvim_create_augroup("vimtex_compile_on_save", { clear = false }),
+  group = vim.api.nvim_create_augroup("vimtex_compile_on_save", { clear = true }),
   pattern = "*.tex",
   callback = function()
     vim.cmd("VimtexCompile")
