c1f0e6d2f5f3b5c170ef32f0188cf486cfa12a0a
Jack Krebsbach <krebsbachjackson@gmail.com>
2025-01-01 09:58:46 -0500

    select mapping

--- lua/config/plugins/blink-cmp.lua
+++ lua/config/plugins/blink-cmp.lua
@@ -5,8 +5,11 @@ return {
     dependencies = 'rafamadriz/friendly-snippets',
     version = '*',
     opts = {
-      keymap = { preset = 'default' },
-
+      keymap = {
+        preset = 'default',
+        ["<C-j>"] = { 'select_and_accept', 'fallback' },
+        ['<C-space>'] = { function(cmp) cmp.show({ providers = { 'snippets' } }) end },
+      },
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono'
